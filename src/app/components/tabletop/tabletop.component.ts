277be5e565493900ab31448b0288fa44ce5c6692
Primal Zed <primal.zed@gmail.com>
2022-04-28 16:14:45 -0500

    sort cards by initiative

--- src/app/components/tabletop/tabletop.component.ts
+++ src/app/components/tabletop/tabletop.component.ts
@@ -5,6 +5,7 @@ import { map } from 'rxjs/operators';
 import { Card } from 'models/card';
 import { AppState } from 'store/app.state';
 import { selectCards } from 'store/tabletop/cards.selector';
+import { CatalogService } from 'services/catalog.service';
 
 @Component({
   templateUrl: './tabletop.component.html',
@@ -24,14 +25,32 @@ export class TabletopComponent {
       })
     );
 
-  public cards$ = this.store.select(selectCards);
+  public cards$ = this.store.select(selectCards)
+    .pipe(
+      map((cards) => cards
+        .slice()
+        .sort((a, b) => this.getInitiative(a) - this.getInitiative(b))
+      )
+    );
 
   constructor(
     private breakpointObserver: BreakpointObserver,
+    private catalogService: CatalogService,
     private store: Store<AppState>
   ) { }
 
   trackCard(index: number, card: Card) {
     return card.key;
+  }
+
+  private getInitiative(card: Card) {
+    switch (card.kind) {
+      case 'character':
+        return card.initiative ?? 0;
+      case 'monster':
+        return card.currentAbilityCardId
+          ? this.catalogService.monsterAbilityCardEntities[card.currentAbilityCardId].initiative
+          : 0
+    }
   }
 }
