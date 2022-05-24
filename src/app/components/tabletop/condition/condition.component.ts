7c1f9bc84a393538bc8bbbf6ce99de3bd19584a7
Primal Zed <primal.zed@gmail.com>
2022-05-23 22:10:11 -0500

    condition colors

--- src/app/components/tabletop/condition/condition.component.ts
+++ src/app/components/tabletop/condition/condition.component.ts
@@ -4,6 +4,10 @@ import { ConditionKey } from 'models/condition';
 @Component({
   selector: 'condition',
   templateUrl: './condition.component.html',
+  styles: [
+    `.gloomhaven-condition-icons { font-size: 2rem; }`,
+    `.overlap { display: inline-grid; > * { grid-column: 1 / 2; grid-row: 1 / 2; } }`
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ConditionComponent {
