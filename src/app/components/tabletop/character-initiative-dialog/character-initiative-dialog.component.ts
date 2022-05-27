eab77c7c2b67437d339b99354a04e81660732316
Primal Zed <primal.zed@gmail.com>
2022-05-27 11:36:28 -0500

    don't ask for initiative of characters with 0 hp

--- src/app/components/tabletop/character-initiative-dialog/character-initiative-dialog.component.ts
+++ src/app/components/tabletop/character-initiative-dialog/character-initiative-dialog.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
 import { AppState } from 'store/app.state';
-import { selectCharacterKeys } from 'store/tabletop/characters/characters.selectors';
+import { selectCharacters } from 'store/tabletop/characters/characters.selectors';
 
 @Component({
   selector: 'app-character-initiative-dialog',
   templateUrl: './character-initiative-dialog.component.html',
-  styleUrls: ['./character-initiative-dialog.component.scss']
+  styleUrls: ['./character-initiative-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CharacterInitiativeDialogComponent {
-  public characterKeys$ = this.store.select(selectCharacterKeys);
+  public characters$ = this.store.select(selectCharacters)
+    .pipe(
+      map((characters) => characters
+        .filter((character) => character.hitPoints)
+      )
+    );
 
   constructor(
     private dialogRef: MatDialogRef<CharacterInitiativeDialogComponent, boolean>,
