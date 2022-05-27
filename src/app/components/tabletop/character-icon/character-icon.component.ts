8d8d049a600228aae1ef0b9a17763dab6eb165db
Primal Zed <primal.zed@gmail.com>
2022-05-27 16:57:40 -0500

    use character icon for stat card header

--- src/app/components/tabletop/character-icon/character-icon.component.ts
+++ src/app/components/tabletop/character-icon/character-icon.component.ts
@@ -3,8 +3,13 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'character-icon',
   templateUrl: './character-icon.component.html',
+  styles: [`:host { display: inline-flex; justify-content: center; }`],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CharacterIconComponent {
   @Input() key!: string;
+
+  @Input() size: 'md' | 'lg' = 'md';
+
+  @Input() shadow: boolean = true;
 }
