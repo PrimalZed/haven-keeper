434b86402d50866974c1b38294d5622b90d98d45
Primal Zed <primal.zed@gmail.com>
2022-05-28 13:13:49 -0500

    small condition size on attack effect

--- src/app/components/tabletop/condition/condition.component.ts
+++ src/app/components/tabletop/condition/condition.component.ts
@@ -4,10 +4,15 @@ import { ConditionKey } from 'models/condition';
 @Component({
   selector: 'condition',
   templateUrl: './condition.component.html',
-  styles: [`:host { display: flex; align-items: center; }`, `img { height: 32px; }`],
+  styles: [
+    `:host { display: flex; align-items: center; }`,
+    `img.sm { height: 24px; }`,
+    `img.md { height: 32px; }`
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ConditionComponent {
   @Input() condition!: ConditionKey
   @Input() showLabel: boolean = false;
+  @Input() size: 'sm' | 'md' = 'md';
 }
